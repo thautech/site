6ae453db54e0cd85ceb2fd6ea03e8a2a969ae046
Thiago Madureira Braga <thiago@thau.tech>
2023-07-05 21:37:13 +0200

    build(project): add TailwindCSS and Icon modules

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Icon name="uil:github" color="black" />
   </div>
 </template>
