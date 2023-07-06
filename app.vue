63fa5a5799c9b32271612ea8ac9999acc36eaaf4
Thiago Madureira Braga <thiago@thau.tech>
2023-07-06 17:02:53 +0200

    build(project): scafold base website

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Icon name="uil:github" color="black" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+      <div>
+        <h1>Welcome to the homepage</h1>
+      </div>
+</template>
+
+<script>
+    definePageMeta({ layout: "default" });
+</script>
+
