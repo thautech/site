92ad1d484bdf1998d73b602fd6395d84d5ff8da8
Thiago Madureira Braga <thiago@thau.tech>
2023-07-11 19:46:13 +0200

    chore(bootstrap): evaluate Bootstrap custom setups

---
+++ assets/bootstrap.js
@@ -0,0 +1,24 @@
+import * as Popper from "../node_modules/@popperjs/core/dist/umd/popper.js";
+
+// from 'js/src/*' source which works
+// import Modal from "../node_modules/bootstrap/js/src/modal";
+// import Tab from "../node_modules/bootstrap/js/src/tab";
+import Dropdown from "../node_modules/bootstrap/js/src/dropdown";
+// import Tooltip from "../node_modules/bootstrap/js/src/tooltip";
+
+export default {
+    Popper,
+    // Alert,
+    // Button,
+    // Carousel,
+    // Collapse,
+    Dropdown,
+    // Modal,
+    // Offcanvas,
+    // Popover,
+    // ScrollSpy,
+    // Tab,
+    // Toast,
+    // Popper,
+    // Tooltip,
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+    // app: {
+    //     head: {
+    //         link: [
+    //             {
+    //                 href: "/bootstrap/bootstrap.min.css",
+    //                 crossorigin: "anonymous",
+    //                 rel: "stylesheet",
+    //             },
+    //         ],
+    //         script: [
+    //             {
+    //                 src: "/bootstrap/bootstrap.bundle.min.js",
+    //                 crossorigin: "anonymous",
+    //             },
+    //         ],
+    //     },
+    // },
     devtools: { enabled: true },
+    modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
 });
