3701d0423b7b55ef374a6ffa9147aa1da4d21138
Thiago Madureira Braga <thiago@thau.tech>
2023-07-12 10:45:18 +0200

    build(project): remove Bootstrap and Tailwind references

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // app: {
-    //     head: {
-    //         link: [
-    //             {
-    //                 href: "/bootstrap/bootstrap.min.css",
-    //                 crossorigin: "anonymous",
-    //                 rel: "stylesheet",
-    //             },
-    //         ],
-    //         script: [
-    //             {
-    //                 src: "/bootstrap/bootstrap.bundle.min.js",
-    //                 crossorigin: "anonymous",
-    //             },
-    //         ],
-    //     },
-    // },
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+    modules: [],
 });
