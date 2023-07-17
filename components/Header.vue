00acbe4649b1c8bc18544b8d237ca2081698a135
Thiago Madureira Braga <thiago@thau.tech>
2023-07-17 17:44:12 +0200

    feat(component): implement sticky header

--- components/Header.vue
+++ components/Header.vue
@@ -1,58 +1,60 @@
 <template>
-    <nav class="container flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="container flex flex-wrap items-center justify-between mx-auto p-4">
 
-        <div class="flex items-center">
-            <div @click="toggleNav" class="flex md:hidden mr-4">
-                <button type="button" class="menu-toggle">
-                    <i-mdi-hamburger-menu class="icon" />
-                </button>
+            <div class="flex items-center">
+                <div @click="toggleNav" class="flex md:hidden mr-4">
+                    <button type="button" class="menu-toggle">
+                        <i-mdi-hamburger-menu class="icon" />
+                    </button>
+                </div>
+                <nuxt-link to="/">
+                    <img src="/images/logo-solid-blue.png" alt="THAU" class="h-12">
+                </nuxt-link>
             </div>
-            <nuxt-link to="/">
-                <img src="/images/logo-solid-blue.png" alt="THAU" class="h-12">
-            </nuxt-link>
-        </div>
 
-        <div class="flex items-center md:order-2">
-            <nav class="inline-flex items-center space-x-2">
-                <a class="text-slate-500 hover:text-slate-900">EN</a>
-                <span>|</span>
-                <a class="text-slate-500 hover:text-slate-900">ES</a>
-            </nav>
-        </div>
+            <div class="flex items-center md:order-2">
+                <nav class="inline-flex items-center space-x-2">
+                    <a class="text-slate-500 hover:text-slate-900">EN</a>
+                    <span>|</span>
+                    <a class="text-slate-500 hover:text-slate-900">ES</a>
+                </nav>
+            </div>
 
-        <div class="items-center w-full md:flex md:w-auto md:order-1" :class="showMenu ? 'flex' : 'hidden'">
-            <ul class="menu-list">
-                <li class="menu-item">PURPOSE</li>
-                <li>
-                    <div class="relative">
-                        <button @click="showServices = !showServices" class="dropdown-toggle">
-                            <span>SERVICES</span><i-mdi-chevron-down class="icon" />
-                        </button>
-                        <div v-show="showServices" class="dropdown-list">
-                            <nuxt-link to="/" class="dropdown-item">Data Science</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Software Development</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Platform Engineering</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Expert Onboard</nuxt-link>
+            <div class="items-center w-full md:flex md:w-auto md:order-1" :class="showMenu ? 'flex' : 'hidden'">
+                <ul class="menu-list">
+                    <li class="menu-item">PURPOSE</li>
+                    <li>
+                        <div class="relative">
+                            <button @click="showServices = !showServices" class="dropdown-toggle">
+                                <span>SERVICES</span><i-mdi-chevron-down class="icon" />
+                            </button>
+                            <div v-show="showServices" class="dropdown-list">
+                                <nuxt-link to="/" class="dropdown-item">Data Science</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Software Development</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Platform Engineering</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Expert Onboard</nuxt-link>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="relative">
-                        <button @click="showArticles = !showArticles" class="dropdown-toggle">
-                            <span>ARTICLES</span><i-mdi-chevron-down class="icon" />
-                        </button>
-                        <div v-show="showArticles" class="dropdown-list">
-                            <nuxt-link to="/" class="dropdown-item">Data Science</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Software Development</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Platform Engineering</nuxt-link>
-                            <nuxt-link to="/" class="dropdown-item">Expert Onboard</nuxt-link>
+                    </li>
+                    <li>
+                        <div class="relative">
+                            <button @click="showArticles = !showArticles" class="dropdown-toggle">
+                                <span>ARTICLES</span><i-mdi-chevron-down class="icon" />
+                            </button>
+                            <div v-show="showArticles" class="dropdown-list">
+                                <nuxt-link to="/" class="dropdown-item">Data Science</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Software Development</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Platform Engineering</nuxt-link>
+                                <nuxt-link to="/" class="dropdown-item">Expert Onboard</nuxt-link>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li class="menu-item">CONTACT</li>
-            </ul>
-        </div>
+                    </li>
+                    <li class="menu-item">CONTACT</li>
+                </ul>
+            </div>
 
+        </div>
     </nav>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,77 @@
 <template>
-    <section class="text-gray-600 body-font">
-        TEST
+    <section class="container flex flex-wrap items-center justify-between mx-auto px-4 pt-24">
+        <h1 class="text-4xl">Mussum</h1>
+        <p class="mb-10">
+            Mussum Ipsum, cacilds vidis litro abertis. A ordem dos tratores não altera o pão duris.Quem num gosta di mim que
+            vai caçá sua turmis!Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis
+            é disculpa de bebadis, arcu quam euismod magna.
+
+            Leite de capivaris, leite de mula manquis sem cabeça.Admodum accumsan disputationi eu sit. Vide electram
+            sadipscing et per.Atirei o pau no gatis, per gatis num morreus.Casamentiss faiz malandris se pirulitá.
+
+            Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.In elementis mé pra quem é amistosis
+            quis leo.Manduma pindureta quium dia nois paga.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis,
+            aguis e fermentis.
+
+            Atirei o pau no gatis, per gatis num morreus.Sapien in monti palavris qui num significa nadis i pareci
+            latim.Quem num gosta di mim que vai caçá sua turmis!Nec orci ornare consequat. Praesent lacinia ultrices
+            consectetur. Sed non ipsum felis.
+        </p>
+
+        <h1 class="text-4xl">Zacarias</h1>
+        <p class="mb-10">
+            Mussum Ipsum, cacilds vidis litro abertis. A ordem dos tratores não altera o pão duris.Quem num gosta di mim que
+            vai caçá sua turmis!Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis
+            é disculpa de bebadis, arcu quam euismod magna.
+
+            Leite de capivaris, leite de mula manquis sem cabeça.Admodum accumsan disputationi eu sit. Vide electram
+            sadipscing et per.Atirei o pau no gatis, per gatis num morreus.Casamentiss faiz malandris se pirulitá.
+
+            Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.In elementis mé pra quem é amistosis
+            quis leo.Manduma pindureta quium dia nois paga.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis,
+            aguis e fermentis.
+
+            Atirei o pau no gatis, per gatis num morreus.Sapien in monti palavris qui num significa nadis i pareci
+            latim.Quem num gosta di mim que vai caçá sua turmis!Nec orci ornare consequat. Praesent lacinia ultrices
+            consectetur. Sed non ipsum felis.
+        </p>
+
+        <h1 class="text-4xl">Didi</h1>
+        <p class="mb-10">
+            Mussum Ipsum, cacilds vidis litro abertis. A ordem dos tratores não altera o pão duris.Quem num gosta di mim que
+            vai caçá sua turmis!Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis
+            é disculpa de bebadis, arcu quam euismod magna.
+
+            Leite de capivaris, leite de mula manquis sem cabeça.Admodum accumsan disputationi eu sit. Vide electram
+            sadipscing et per.Atirei o pau no gatis, per gatis num morreus.Casamentiss faiz malandris se pirulitá.
+
+            Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.In elementis mé pra quem é amistosis
+            quis leo.Manduma pindureta quium dia nois paga.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis,
+            aguis e fermentis.
+
+            Atirei o pau no gatis, per gatis num morreus.Sapien in monti palavris qui num significa nadis i pareci
+            latim.Quem num gosta di mim que vai caçá sua turmis!Nec orci ornare consequat. Praesent lacinia ultrices
+            consectetur. Sed non ipsum felis.
+        </p>
+
+        <h1 class="text-4xl">Dede</h1>
+        <p class="mb-10">
+            Mussum Ipsum, cacilds vidis litro abertis. A ordem dos tratores não altera o pão duris.Quem num gosta di mim que
+            vai caçá sua turmis!Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Copo furadis
+            é disculpa de bebadis, arcu quam euismod magna.
+
+            Leite de capivaris, leite de mula manquis sem cabeça.Admodum accumsan disputationi eu sit. Vide electram
+            sadipscing et per.Atirei o pau no gatis, per gatis num morreus.Casamentiss faiz malandris se pirulitá.
+
+            Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.In elementis mé pra quem é amistosis
+            quis leo.Manduma pindureta quium dia nois paga.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis,
+            aguis e fermentis.
+
+            Atirei o pau no gatis, per gatis num morreus.Sapien in monti palavris qui num significa nadis i pareci
+            latim.Quem num gosta di mim que vai caçá sua turmis!Nec orci ornare consequat. Praesent lacinia ultrices
+            consectetur. Sed non ipsum felis.
+        </p>
+
     </section>
 </template>
 
