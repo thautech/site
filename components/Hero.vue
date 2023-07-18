3e29c229ee612fe939eacd88e9398bce924a27f9
Thiago Madureira Braga <thiago@thau.tech>
2023-07-18 12:33:20 +0200

    feat(components): scafold base components

---
+++ components/Hero.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="container flex flex-wrap items-center justify-between mx-auto px-4 pt-20">
+        HERO
+    </section>
+</template>
---
+++ components/Section.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="container flex flex-wrap items-center justify-between mx-auto px-4">
+
+        <div>
+            <h2>{{ title }}</h2>
+        </div>
+
+        <div>
+            <img :src="image">
+        </div>
+
+        <div>
+            <slot />
+        </div>
+
+    </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    title?: string
+    image?: string
+}>()
+</script>
